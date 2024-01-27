6dc5e5c2d159aa0dc15743ce8c2b52834706d960
Imtiaz Kabir <100441548+imtiazkabir496@users.noreply.github.com>
2024-01-27 06:31:53 +0000

    datatype

---
+++ 01_basic/dataType.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer vertion
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true, false
+// null =>  standalon value
+// undefined => 
+// symbol => unique
+
+
+
+// object => 
+console.log(typeof undefined); // object type
+console.log(typeof undefined); // undefined type
