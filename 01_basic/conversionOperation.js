01f87bf9f9c86472c2cc7e0c2cbd7912b481c051
Imtiaz Kabir <100441548+imtiazkabir496@users.noreply.github.com>
2024-01-27 07:25:26 +0000

    converion

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,21 @@
+let score = undefined
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+//
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 45
+let stringConvert = String(someNumber)
+
+console.log(typeof stringConvert);
